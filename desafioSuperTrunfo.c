f9d3676dfe302a3df18137b60f913ad1a354f8d0
valclebiorodgs <147004595+valclebiorodgs@users.noreply.github.com>
2025-03-24 21:43:50 +0000

    novo commit

--- desafioSuperTrunfo.c
+++ desafioSuperTrunfo.c
@@ -4,7 +4,7 @@ int main (){
     // Definição das variáveis para armazenar as propriedades das cidades
     char codigo_carta[10], codigo_carta02[10];
     char nome_cidade[50], nome_cidade02[50];
-    char estado, estado02;  // Espera-se que o usuário digite apenas UMA letra
+    char estado[20], estado02[20]; 
     int populacao, populacao02;
     float area, area02;
     float pib, pib02;
@@ -18,7 +18,7 @@ int main (){
     scanf("%s", codigo_carta);  // Lê a string para o código da carta
 
     printf("Estado (letra de A a H): ");
-    scanf(" %c", &estado);   // Lê apenas UM caractere; se o usuário digitar mais, os demais ficarão no buffer!
+    scanf(" %s", &estado);   
 
     printf("Nome da cidade: ");
     scanf("%s", nome_cidade);  // Lê apenas uma palavra para o nome da cidade
@@ -42,7 +42,7 @@ int main (){
     scanf("%s", codigo_carta02);
 
     printf("Estado  (letra de A a H): ");
-    scanf(" %c", &estado02);   
+    scanf(" %s", &estado02);   
 
     printf("Nome da cidade: ");
     scanf("%s", nome_cidade02); 
@@ -59,4 +59,52 @@ int main (){
     printf("Quantidade de pontos turisticos: ");
     scanf("%d", &qtde_pontos_turisticos02);
 
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("CARTA 01:\n");
+    printf("Codigo: %s\n", codigo_carta);
+    printf("Estado: %c\n", estado);
+    printf("Nome da Cidade: %s\n", nome_cidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km2\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Quantidade de pontos turisticos: %d\n", qtde_pontos_turisticos);
+
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("CARTA 02:\n");
+    printf("Codigo: %s\n", codigo_carta02);
+    printf("Estado: %c\n", estado02);
+    printf("Nome da Cidade: %s\n", nome_cidade02);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km2\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Quantidade de pontos turisticos: %d\n", qtde_pontos_turisticos);
+
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("Comparação de cartas (Atributo: População):\n");
+
+
+
+    //Calculo da densidade populacional (Carta 01):
+    float densidade_pop01 = populacao / area;
+    printf("A densidade populacional da Carta 01 é: %f \n", densidade_pop01);
+    
+    // Calculo da densidade populacional (Carta 02):
+    float densidade_pop02 = populacao02 / area02;
+    printf("A densidade populacional da Carta 02 é: %f \n", densidade_pop02);
+
+
+
+    // Comparar a população de ambas as cartas
+    if (populacao > populacao02){
+        printf("Carta 1 - %s - Populaçao: %d \n", nome_cidade, populacao);
+        printf("Carta 2 - %s - Populaçao: %d\n", nome_cidade02, populacao02);
+        printf("Resultado: Carta 1 VENCEU!\n");
+    } else{
+        printf("Carta 1 - %s - Populaçao: %d\n", nome_cidade, populacao);
+        printf("Carta 2 - %s - Populaçao: %d\n", nome_cidade02, populacao02);
+        printf("Resultado: Carta 2 VENCEU!");
+    }
+
+
+    return 0;
 }
